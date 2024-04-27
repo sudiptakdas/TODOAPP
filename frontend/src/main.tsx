060367647f399221a186cb1440044e3e35f24726
Sudipta Kumar Das <sudiptadas2000k@gmail.com>
2024-04-27 16:19:46 +0530

    main:Added Readme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import TodoList from './pages/TodoList.tsx';
 import './index.css';
 import store from './redux/store.ts';
@@ -10,10 +9,6 @@ import { Provider } from 'react-redux';
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
-  },
-  {
-    path: '/todo',
     element: <TodoList />,
   },
 ]);
